0eedefa0a8df15de0ddb8cb2b78128b5e9f27b9c
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-10-03 18:19:25 +0100

    yx

--- task_management_system/task_management_system/settings.py
+++ task_management_system/task_management_system/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'task_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # This includes your custom templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
